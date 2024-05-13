bc56ef7e1889915fb8fbc874e7fc0c6cac64d884
Djorkastillo <djcastillov@correo.url.edu.gt>
2024-05-13 11:08:22 -0600

    Create LT9DJCV.py

---
+++ PYTHON - DJCV 1302824/LT9DJCV.py
@@ -0,0 +1,11 @@
+#Asigancion de las variables
+variable1 = "Hola "
+variable2 = 0
+variable3 = True
+#Operaciones  basicas
+suma = 3+2
+resta = 3-2
+multiplicacion = 2*3
+# este es mi comentario
+nombre = input("Ingrese su nombre: ")
+print(variable1+nombre)
